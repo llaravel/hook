27821e09c2cfd06c89ea5e107c414697de47c2b1
llaravel <oscode@qq.com>
2023-03-21 22:47:42 +0800

    Update ServiceProvider.php
    xxx

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -3,7 +3,6 @@
 
 namespace Llaravel\Hook;
 
-use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Config;
 use Illuminate\Support\ServiceProvider as LaravelServiceProvider;
 class ServiceProvider extends LaravelServiceProvider
@@ -14,18 +13,18 @@ class ServiceProvider extends LaravelServiceProvider
     public function boot()
     {
     }
-
+	protected function init()
+	{
+		
+	}
     /**
      * 读取配置文件
      */
     protected function hookConfig()
     {
-        var_dump("这里是我的代码哦");
         $configs=Config::get('hooks');
         if(!$configs) return;
-        //'cache.index'='wechat.official_account.default'
         foreach($configs as $key =>$val){
-            $value=Cache::get($key);
             Config::set($val,$value);
         }
     }
